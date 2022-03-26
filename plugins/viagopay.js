fcca5e82f3a2f7abe0e2837bf2bd718dc1529a71
BunyanXd <101381544+BunyanXd@users.noreply.github.com>
2022-03-26 19:02:43 +0700

    Update viagopay.js

--- plugins/viagopay.js
+++ plugins/viagopay.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via GOPAY
-╠➥ *085730903853*
+╠➥ *6281380311018*
 ║- Chat Owner Terlebebih
 ║ Dahulu
 ╠═〘 Fitur 〙 ═
